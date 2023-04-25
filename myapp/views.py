1d06831b212bce30f680285e18c7da4a0749b1e0
DiegoVF1391 <19121092@morelia.tecnm.mx>
2023-04-24 20:09:49 -0600

    Guardar datos principales de las recetas en una base de datos

--- myapp/views.py
+++ myapp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ImageForm
 import pytesseract
-from PIL import Image
+import re
 import os
-import numpy as np
-import cv2
+from datetime import datetime
+from myapp.models import Cita
+from .forms import ImageForm
+from PIL import Image
 
 def dashboard(request):
     return render(request, 'dashboard.html')
@@ -16,20 +18,36 @@ def upload_image(request):
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.cleaned_data['image']
-            # Leer la imagen en formato numpy array utilizando OpenCV
-            img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-            # Convertir la imagen a escala de grises
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # Aplicar binarización adaptativa para mejorar el contraste de la imagen
-            gray = cv2.medianBlur(gray, 3)
-            #cv2.imshow('Image',gray)
-            #cv2.waitKey(0)
-            #cv2.destroyAllWindows()
-            # Extraer el texto utilizando pytesseract
-            text = pytesseract.image_to_string(gray, lang='spa')
+            # Guardar la imagen temporalmente
+            with open('temp_image.jpg', 'wb+') as f:
+                for chunk in image.chunks():
+                    f.write(chunk)
+            # Procesar la imagen utilizando pytesseract
+            img = Image.open('temp_image.jpg')
+            text = pytesseract.image_to_string(img, lang='spa')
+            # Eliminar la imagen temporal
+            f.close()
+            os.remove('temp_image.jpg')
             # Mostrar el texto extraído en la consola
             text = text.replace("º", "o")
             print(text)
+
+            # Procesar el texto para extraer la información relevante
+            fecha_match = re.search(r'Fecha: (\d{2}/\d{2}/\d{4})', text)
+            padecimiento_match = re.search(r'Padecimiento: (.+)', text)
+
+            print(fecha_match, padecimiento_match)
+            if fecha_match and padecimiento_match:
+            
+                # Crear una instancia del modelo Receta
+                fecha_cita = datetime.strptime(fecha_match.group(1), '%d/%m/%Y')
+                padecimiento_cita = padecimiento_match.group(1)
+                print(fecha_cita, padecimiento_cita)
+                cita = Cita(fecha_cita=fecha_cita, padecimiento=padecimiento_cita, paciente_id=1)
+                #print(cita)
+                ## Guardar la instancia en la base de datos
+                cita.save()
+                
             # Redirigir a la página de dashboard
             return redirect('dashboard')
     else:
